acc6053278eb59a7eece369aec602ea37e2dc9a9
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-04 17:38:05 -0700

    c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- * _memcpy - fills memory with another buffer.
- * @dest: source string
- * @src: string for filling
- * @n: lenght of buffer
- * Return: new string.
+ * _memcpy - function that copies memory area
+ *@dest: the address of memory to print
+ *@src: second value
+ *@n: thirth value
+ *
+ * Return: char with result of memset
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int x = 0
-for (x = 0; x < n; x++)
-{
-dest[x] = src[x];
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
-return (dest);
-}
+
