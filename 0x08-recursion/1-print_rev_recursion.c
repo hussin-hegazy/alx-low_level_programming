e50948d41580364d7c226071fdc046712888b0d8
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-05 12:59:08 -0700

    c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- *_puts_recursion - Print a text string
+ *_print_rev_recursion - Print a text string
  *@s: text string
  *
  *
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 if (*s == 0)
 {
 putchar('\n');
 return;
 }
-_puts_recursion(s + 1);
+_print_rev_recursion(s + 1);
 putchar(*s);
 }
