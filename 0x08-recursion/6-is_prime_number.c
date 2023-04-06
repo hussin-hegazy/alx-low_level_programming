13e7e99c3bb7f8a1c918348b448e2f7665c0a408
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-05 19:48:15 -0700

    c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+int _primo(int num_, int inf);
+
+/**
+ * is_prime_number - function that  returns 1 if the input integer is a prime.
+ *@n: first value
+ *
+ * Return: Return solution
+ */
+int is_prime_number(int n)
+{
+	int a = 2;
+
+	if (n == 1)
+		return (0);
+	if (n < 0)
+		return (0);
+	return (_primo(n, a));
+}
+
+/**
+ *_primo - function that returns 1 if the input integer is a prime.
+ *@x: first value
+ *@y: second value
+ *
+ * Return: Return solution
+ */
+int calculo_primo(int x, int y)
+{
+	if (x / 2 < y)
+		return (1);
+	if (x % y == 0)
+		return (0);
+	return (_primo(x, y + 1));
+}
