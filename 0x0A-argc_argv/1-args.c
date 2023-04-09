d92f5b1f575b20cfbde7d8f3cd31131becc50ae3
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-09 15:06:50 -0700

    cccc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function returning an integer.
+ *@argc: argument c
+ *@argv: argument v
+ *Return: Success
+ */
+int main(int argc, char *argv[])
+{
+printf("%i\n", argc);
+return (0);
+}
