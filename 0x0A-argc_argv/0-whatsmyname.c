a5c8b1a541b950c6e502cc276215df06b072bd66
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-09 14:52:00 -0700

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function returning an integer.
+ *@argc: argument c
+ *@argv: argument v
+ *Return: Success
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
