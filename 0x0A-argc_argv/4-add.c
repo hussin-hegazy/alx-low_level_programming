310b1d00c61a1a6557ab4208cd67efb1d7825a83
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-10 19:03:37 -0700

    c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
  *main - function returning an integer.
  *@argc: argument c
- *
  *@argv: argument v
  *Return: Success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 int sum = 0;
+int num;
+char *endptr;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
 for (int i = 1; i < argc; i++)
 {
-for (int j = 0; argv[i][j]; j++)
-{
-if (!isdigit(argv[i][j]))
+num = strtol(argv[i], &endptr, 10);
+if (*endptr != '\0')
 {
 printf("Error\n");
 return (1);
 }
+if (num > 0)
+{
+sum += num;
 }
-sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
 return (0);
