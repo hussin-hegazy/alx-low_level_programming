883ab2e743968aa0d7b472b89492184b7636c6f8
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-11 16:43:41 -0700

    c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,19 +11,19 @@
 char *create_array(unsigned int size, char c)
 {
 int y;
-char *xarray;
+char *x;
 if (size == 0)
 {
 return (NULL);
 }
-xarray = malloc(sizeof(char) * size);
-if (xarray == 0)
+x = malloc(sizeof(char) * size);
+if (x == 0)
 {
 return (NULL);
 }
 for (y = 0; y < size; y++)
 {
-xarray[y] = c;
+x[y] = c;
 }
-return (xarray);
+return (x);
 }
