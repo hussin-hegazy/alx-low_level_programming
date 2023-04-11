e4e511fdab741de867f4588c15b943d67a560a0c
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-10 18:47:47 -0700

    C

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,14 +8,12 @@
  *@argv: argument v
  *Return: Success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 int sum = 0;
-int i;
-for (i = 1; i < argc; i++)
+for (int i = 1; i < argc; i++)
 {
-int j;
-for (j = 0; argv[i][j] != '\0'; j++)
+for (int j = 0; argv[i][j]; j++)
 {
 if (!isdigit(argv[i][j]))
 {
@@ -23,11 +21,7 @@ printf("Error\n");
 return (1);
 }
 }
-int num = atoi(argv[i]);
-if (num > 0)
-{
-sum += num;
-}
+sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
 return (0);
