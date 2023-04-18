051c437b7cf5aa1bc5ef5c087481cfb2a204e4a8
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-18 00:16:33 -0700

    c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,6 +4,8 @@
  *free_dog - function that free dogs
  *@d: pointer strct
  *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void free_dog(dog_t *d)
 {
