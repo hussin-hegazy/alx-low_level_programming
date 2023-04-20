3129c30eeffd8d04f0467a18169e93c60947cacf
hussin-hegazy <hussinhegazy350gmail.com>
2023-04-19 19:59:59 -0700

    c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: String
+ * @f: Pointer to function with char parameter and no return
+ *
+ * Return:NA
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != 0)
+		f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * array_iterator - main function execute a function given an array
+ * @array: first value
+ * @size: function 1
+ * @action: function 2
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t y;
+
+	if (array == NULL || size == 0 || action == NULL)
+		return;
+	for (y = 0; y < size; y++)
+	{
+		action(array[y]);
+	}
+}
