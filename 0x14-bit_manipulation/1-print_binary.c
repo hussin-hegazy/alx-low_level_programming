2809959d712e0cf2e9898bbf75918e2e9e4119e7
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-04 16:23:53 -0700

    c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,7 +12,7 @@ void print_binary(unsigned long int n)
 unsigned long int mlb = 1UL << (sizeof(n) * 8 - 1);
 while (mlb)
 {
-putchar((n & mlb) ? '1' : '0');
+_putchar((n & mlb) ? '1' : '0');
 mlb >>= 1;
 }
 }
