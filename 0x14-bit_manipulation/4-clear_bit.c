18fe2ace86fbc1e6aabbe257a8b128b1accf712c
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-04 17:52:43 -0700

    c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * set_bit - sets the value of a bit to 1 at a given index
+ * clear_bit - sets the value of a bit to 1 at a given index
  * @n: base 10 number
  * @index: index to set to 1
  *
  * Return: 1 if it worked, or -1 if an error occurred
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(*n) * 8)
 return (-1);
