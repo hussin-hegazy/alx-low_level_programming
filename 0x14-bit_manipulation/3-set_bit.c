a5a2f5eb5067d3264e920f09b7ccbf88d903a5d2
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-04 17:09:25 -0700

    c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+int bt;
+if (index >= sizef(*n) * 8)
+return (-1);
+bt = n | (1 << index);
+return (1);
+}
