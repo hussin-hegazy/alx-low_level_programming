5b0a66e0e69599c0d20d76846b0e14076e10cdde
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-12 06:58:15 -0700

    c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,45 @@
-#include "main,h"
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
+
 /**
- * read_textfile -  reads a text file and prints it
- * to the POSIX standard output
- * @filename: name of the file
- * @letters: number of letters it should read and print
+ * read_textfile -  reads text file and prints to the POSIX std output
+ * @filename: name of the file to read
+ * @letters: number of the letters to print
  *
- * Return: returns the actual number of letters it could read and print
- * 0 if file cannot be opened or read
- * 0 if filename is NULL
- * 0 if write fails or does not write expected amount of bytes
+ * Return: actual number of letters it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t n_read, n_written;
-    char *buffer;
+	int fd = 0, fdr = 0, fdw = 0;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
-
-    n_read = read(fd, buffer, letters);
-    if (n_read == -1)
-        return (0);
-
-    n_written = write(STDOUT_FILENO, buffer, n_read);
-    if (n_written == -1 || n_written != n_read)
-        return (0);
-
-    close(fd);
-    free(buffer);
-    return (n_written);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fdr = read(fd, buf, letters);
+	if (fdr == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	fdw = write(STDOUT_FILENO, buf, fdr);
+	if (fdw == -1 || fdw != fdr)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (fdr);
 }
