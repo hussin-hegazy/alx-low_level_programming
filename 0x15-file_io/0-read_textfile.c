fc8c81da75b7d95c1c0b5ad6ef3ae175de9ffd01
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-12 06:54:26 -0700

    c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,6 @@
-#include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "main,h"
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 /**
  * read_textfile -  reads a text file and prints it
@@ -17,24 +15,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-size_t red, wrt;
-char *bf;
-if (filename == NULL)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-bf = malloc(sizeof(char) * letters);
-if (bf == NULL)
-return (0);
-red = read(fd, bf, letters);
-if (red == -1)
-return (0);
-wrt = write(STDOUT_FILENO, bf, red);
-if (wrt == -1 || wrt != red)
-return (0);
-close(fd);
-free(bf);
-return (wrt);
+    int fd;
+    ssize_t n_read, n_written;
+    char *buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
+
+    n_read = read(fd, buffer, letters);
+    if (n_read == -1)
+        return (0);
+
+    n_written = write(STDOUT_FILENO, buffer, n_read);
+    if (n_written == -1 || n_written != n_read)
+        return (0);
+
+    close(fd);
+    free(buffer);
+    return (n_written);
 }
