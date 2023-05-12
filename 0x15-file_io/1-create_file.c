7f0c525803f1a4ecb1e5d34d301fb3d7a9a6969f
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-12 09:31:43 -0700

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,17 +21,20 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int i =0, wrt;
+int i = 0, wrt;
 if (filename == NULL)
 return (-1);
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd ==  -1)
 return (-1);
+if (text_content)
+{
 while (text_content[i] != 0)
 i++;
 wrt = write(fd, text_content, i);
 if (wrt == -1)
 return (-1);
+}
 close(fd);
 return (1);
 }
