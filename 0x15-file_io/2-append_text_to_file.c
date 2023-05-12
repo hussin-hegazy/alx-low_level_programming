ffa51b3f2d6c5930a8e72c4e70ca89713a78dad0
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-12 11:50:03 -0700

    c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,26 +6,27 @@
 #include <stdlib.h>
 
 /**
- * create_file - creates a file
+ * append_text_to_file - function that appends text at the end of a file
  *
  * @filename: name of the file
- * @text_content: NULL terminated string to write to the file
+ * @text_content: NULL terminated string to append to end of file
  *
  * Return: Returns: 1 on success, -1 on failure
- * file can not be created, file can not be written, write “fails”, etc…
- * The created file must have those permissions: rw-------.
- * If the file already exists, do not change the permissions.
+ * Do not create the file if it does not exit
+ *
  * if filename is NULL return -1
- * if text_content is NULL create an empty file
+ * If text_content is NULL, do not add anything to the file.
+ * Return 1 if the file exists and -1 if the file does not exist or if you
+ * do not have the required permissions to write the file
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, checkw, l = 0;
 
 	if (filename == 0)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
@@ -34,6 +35,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[l] != 0)
 			l++;
+
 		checkw = write(fd, text_content, l);
 
 		if (checkw == -1)
