f3028090c68a3fb48ddd2a967a82f25012126f5a
hussin-hegazy <hussinhegazy350@gmail.com>
2023-05-12 09:44:52 -0700

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,45 +1,40 @@
-#include "main.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * create_file - creates a file
+ * create_file - create a file
+ * @filename: name of the file to create
+ * @text_content: text to insert in the file
  *
- * @filename: name of the file
- * @text_content: NULL terminated string to write to the file
- *
- * Return: Returns: 1 on success, -1 on failure
- * file can not be created, file can not be written, write “fails”, etc…
- * The created file must have those permissions: rw-------.
- * If the file already exists, do not change the permissions.
- * if filename is NULL return -1
- * if text_content is NULL create an empty file
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, checkw, l = 0;
+	int fd = 0, fdw = 0, count = 0;
 
-	if (filename == 0)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content == NULL)
+		text_content = "";
+	while (text_content[count] != '\0')
+		count++;
+
+	fdw = write(fd, text_content, count);
+	if (fdw == -1)
 	{
-		while (text_content[l] != 0)
-			l++;
-		checkw = write(fd, text_content, l);
-
-		if (checkw == -1)
-			return (-1);
+		close(fd);
+		return (-1);
 	}
-
 	close(fd);
 	return (1);
 }
